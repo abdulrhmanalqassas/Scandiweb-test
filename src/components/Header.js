aef97654ca41abc3145b0a4e850654674d45a8fa
abdulrhmanalqassas <46942535+abdulrhmanalqassas@users.noreply.github.com>
2022-06-22 20:28:57 +0200

    start

---
+++ src/components/Header.js
@@ -0,0 +1,13 @@
+import { PureComponent } from 'react';
+
+export class Main extends PureComponent {
+    render() {
+        return (
+            <p>
+                We have overridden the ProductCard component!
+            </p>
+        );
+    }
+}
+
+export default Main;
