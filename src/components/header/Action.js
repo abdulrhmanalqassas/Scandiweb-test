01463e2875f270db7f7e9b85067d5140c32cf5ee
abdulrhmanalqassas <46942535+abdulrhmanalqassas@users.noreply.github.com>
2022-06-23 19:53:02 +0200

    cart icon

--- src/components/header/Action.js
+++ src/components/header/Action.js
@@ -1,15 +1,15 @@
 import { PureComponent } from "react";
-
+import cartIcon from "../../images/cart.png";
 export class Action extends PureComponent{
     render(){
         return(
             <div className='nav-action'>
                     <div className='nav-currency'>
-                        
-<h3>nav </h3>
+
+            `   <h3>nav </h3>
                     </div>
                     <div className='nav-cart'>
-
+                        <img src={cartIcon} alt="cart" />
                     </div>
             </div>
         )
