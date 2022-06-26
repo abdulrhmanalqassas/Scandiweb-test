44a32def7fdb620e1bd2bc7740772980afc5aefb
abdulrhmanalqassas <46942535+abdulrhmanalqassas@users.noreply.github.com>
2022-06-26 20:07:11 +0200

    Currency Switcher dro and cart logo

---
+++ src/components/category/Category.js
@@ -0,0 +1,7 @@
+import React, { PureComponent } from "react";
+
+export default class Category extends PureComponent{
+    render() {
+        return (<h1>Category name</h1>)
+    }
+}
--- src/components/header/Action.js
+++ src/components/header/Action.js
@@ -1,67 +1,49 @@
 import { PureComponent } from "react";
-import {gql } from "@apollo/client";
+import { gql } from "@apollo/client";
 import cartIcon from "../../images/cart.png";
-// import { QueryClient, QueryClientProvider,useQuery } from "react-query";
-// import { QueryClientHook } from 'react-query-class-component';
-import c from "../../client";
-import { Query, Mutation, Subscription } from '@apollo/client/react/components';
-import { graphql } from '@apollo/client/react/hoc';
+import { Query, Mutation, Subscription } from "@apollo/client/react/components";
+import { graphql } from "@apollo/client/react/hoc";
 
-const client = c()  
+const GET_CURRENCIES = gql`
+  query {
+    currencies {
+      label
+      symbol
+    }
+  }
+`;
 
-const GET_CURRENCIES =gql`
-query{
-	currencies{
-		label
-    symbol
+export class Action extends PureComponent {
+  render() {
+    return (
+      <div className="nav-action">
+        <div className="dropdown">
+          <div className="dropbtn">$<div className="dropdown-arrow"></div></div>
+          
+          <div className="dropdown-content">
+            <Query query={GET_CURRENCIES}>
+              {({ loading, error, data }) => {
+                if (error) return <h1>Error...</h1>;
+                if (loading || !data) return <h1>Loading...</h1>;
+                console.log(data);
+                const { currencies } = data;
+
+                return currencies.map((elem) => (
+                  <div>
+                    {elem.symbol}
+                    {elem.label}
+                  </div>
+                ));
+              }}
+            </Query>
+          </div>
+
+        </div>
+        
+       <div className="nav-cart"><img src={cartIcon}></img></div> 
+      </div>
+    );
   }
 }
-`
-
- let ali =client
-  .query({
-    query: GET_CURRENCIES
-  })
-  .then(data => data.data.currencies)
- ali.then(data=>data)
-
-
-  function getJobs() {
-    return new Promise((resolve, reject) => {
-     client
-      .query({
-       query:GET_CURRENCIES
-      })
-      .then(resolve)
-      .catch(reject);
-    });
-   }
-    let valu=  getJobs().then(data=>data.data.currencies)
-
-    async function getarr(){
-        const res = await valu
-        const data = await res
-        console.log( data   )
-     }
-   
-export class Action extends PureComponent{
-    
-      
-   
-    
-    
-    render(){
-       return(
-<Query query={GET_CURRENCIES}>
-{({ loading, error, data }) => {
-            if (error) return <h1>Error...</h1>;
-            if (loading || !data) return <h1>Loading...</h1>;
-
-            return <h1>{data.data}</h1>
-        }}
-</Query>
-        )
-    }
-} 
 
 export default Action;
