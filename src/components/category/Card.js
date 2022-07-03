b037fffcd193038e567773edb438cdf8f3e99ad8
abdulrhmanalqassas <46942535+abdulrhmanalqassas@users.noreply.github.com>
2022-07-03 21:19:40 +0200

    card

--- src/components/category/Card.js
+++ src/components/category/Card.js
@@ -1,7 +1,20 @@
 
-import React from "react";
+import React, { PureComponent } from "react";
 
 //prop
 // img
 // name 
 // price 
+
+export default class Card  extends PureComponent{
+    
+    render(){
+        return(
+            <div className="category-card">
+                <h1>{this.props.key}</h1>
+                <h1>{this.props.titel}</h1>
+               
+            </div>
+        )
+    }
+}
--- src/components/category/Category.js
+++ src/components/category/Category.js
@@ -1,14 +1,47 @@
 import React, { PureComponent } from "react";
+import Card from "./Card";
+import { gql } from "@apollo/client";
+import { Query} from "@apollo/client/react/components";
 
-
+const GET_PRODUCTS = gql`
+query {
+    category {
+     
+      products {
+        id
+        name
+        inStock
+        gallery
+      }
+    }
+  }
+  
+`;
 
 export default class Category extends PureComponent{
     render() {
         return (
             <div className="category" >
-        <h1 className="category-title" >Category name</h1>\
+        <h1 className="category-title" >Category name</h1>
         <div className="category-card-contanir">
-            
+        <Query query={ GET_PRODUCTS}>
+        {({ loading, error, data }) => {
+                if (error) return <h1>Error...</h1>;
+                if (loading || !data) return <h1>Loading...</h1>;
+                console.log(data.category.products);
+                const  products = data.category.products;
+
+                return products.map((elem) => (
+                  <Card
+                   key={elem.id} 
+                   titel = {elem.name}
+                   inStock =  {elem.inStock}
+                   gallery = {elem.gallery}
+                   />
+                )
+                );
+              }}
+        </Query>
         </div>
         </div>
         )
