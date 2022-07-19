ae81280b4beefc9c44b5eda2aa189e78ebe4a751
abdulrhmanalqassas <46942535+abdulrhmanalqassas@users.noreply.github.com>
2022-07-19 20:52:39 +0200

    GET_PRICE query

--- src/components/category/Card.js
+++ src/components/category/Card.js
@@ -1,26 +1,57 @@
-
 import React, { PureComponent } from "react";
-import  Common from "../../images/Common.png"
+import Common from "../../images/Common.png";
+import { gql } from "@apollo/client";
+import { Query } from "@apollo/client/react/components";
 //prop
 // img
-// name 
-// price 
+// name
+// price
 
-export default class Card  extends PureComponent{
-    
-    render(){
-        return(
-            <div className="category-card">
-
-                <div>
-                <img className="category-card-img" src={this.props.gallery[0]} alt={this.props.titel}/>
-                <img className="category-card-icon" src={Common}/>
-                </div>
-                
-                <h2>{this.props.titel}</h2>
-                <h3>price</h3>
-               
-            </div>
-        )
+const GET_PRICE = gql`
+  query {
+    product(id: "huarache-x-stussy-le") {
+      prices {
+        currency {
+          label
+        }
+        amount
+      }
     }
+  }
+`;
+
+export default class Card extends PureComponent {
+  render() {
+    return (
+      <div className="category-card">
+        <div>
+          <img
+            className="category-card-img"
+            src={this.props.gallery[0]}
+            alt={this.props.titel}
+          />
+          <img className="category-card-icon" src={Common} />
+        </div>
+
+        <h2>{this.props.titel}</h2>
+        <Query query={GET_PRICE}>
+
+
+        {({ loading, error, data }) => {
+          if (error) return <h1>Error...</h1>;
+          if (loading || !data) return <h1>Loading...</h1>;
+          {console.log(">>>>>>>>>>>",data.product.prices[0].amount)}
+                return(
+                    <h1>
+                    {data.product.prices[0].amount}
+                </h1>
+                )
+                
+        }}
+
+        </Query>
+       
+      </div>
+    );
+  }
 }
